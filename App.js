b5aaeb07a1d5bcce87b80bf94f26a7f2f8dd0f70
christopher <u15022392@tuks.co.za>
2022-02-21 12:15:06 +0200

    created Landing page Branch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Our Events</Text>
+      <Text>Our Events -- Landning page</Text>
       <StatusBar style="auto" />
     </View>
   );
